967683a8b47f7bb0e42e16f593b9b4ef83f9a969
Divyanshu Shukla <75030391+DivyanshuShukla001@users.noreply.github.com>
2023-09-14 00:12:59 +0530

    Update RadarCalc.cpp

--- RadarCalc.cpp
+++ RadarCalc.cpp
@@ -1,109 +1,23 @@
 #include <iostream>
-#include <conio.h>
-#include <cmath>
-
 using namespace std;
 
-int main() {
+double radarRangeCalculator(double time) {
+    // Speed of light in meters per second
+    const double speedOfLight = 299792458.0;
 
-int radar_range =  180000;
-int intial_range = 99273;
+    // Calculate distance using formula: distance = (speed * time) / 2
+    double distance = (speedOfLight * time) / 2;
 
-int main_site;
-int site_two;
-int site_three;
-int site_four;
-
-cout << "(Please read MAIN.README in GIT Respository )" << "\n\n\n\n\n\n\t\t\t";
-
-cout << "Enter Main Site value : " ;
-cin >> main_site;
-cout << "Now Enter Site range where you are Targeting :"; 
-cin >> site_two, site_three, site_four;
-
- 
-for  (int i = 0; i < main_site; i++) {
-     for (int x = 1; x < site_two; x++) {
-         for (int y = 2; y < site_three; y++) {
-             for (int o = 3; o < site_four; o++) {
-                         if (i + x + y + o == 6) {
-                            cout << "Enter intial range: ";
-                             cin >> intial_range;
-
-                         }                 
-             }
-         }
-     }
+    return distance;
 }
-
-if (cin >> site_two ) {
-    cout <<"Radar impulsive range ~~> ";
-     cout << sqrt(site_two * intial_range + 910 - 83);  
-}
-
-if (cin >> site_three) {
-    cout << "Radar impulsive to double range ~~> ";
-      cout << sqrt(site_three * radar_range + intial_range / 910 + 83);
-}
-
-
-if (cin >> site_four) {
-    cout << "Radar impulsive to double range ~~> ";
-      cout << sqrt(site_four * radar_range + intial_range / 910 + 83);
-}
-
-}#include <iostream>
-#include <conio.h>
-#include <cmath>
-
-using namespace std;
 
 int main() {
+    double timeToTarget;
+    cout << "Enter round-trip time in seconds: ";
+    cin >> timeToTarget;
 
-int radar_range =  180000;
-int intial_range = 99273;
+    double distanceToTarget = radarRangeCalculator(timeToTarget);
+    cout << "The distance to the target is approximately " << distanceToTarget << " meters." << endl;
 
-int main_site;
-int site_two;
-int site_three;
-int site_four;
-
-cout << "(Please read MAIN.README in GIT Respository )" << "\n\n\n\n\n\n\t\t\t";
-
-cout << "Enter Main Site value : " ;
-cin >> main_site;
-cout << "Now Enter Site range where you are Targeting :"; 
-cin >> site_two, site_three, site_four;
-
- 
-for  (int i = 0; i < main_site; i++) {
-     for (int x = 1; x < site_two; x++) {
-         for (int y = 2; y < site_three; y++) {
-             for (int o = 3; o < site_four; o++) {
-                         if (i + x + y + o == 6) {
-                            cout << "Enter intial range: ";
-                             cin >> intial_range;
-
-                         }                 
-             }
-         }
-     }
-}
-
-if (cin >> site_two ) {
-    cout <<"Radar impulsive range ~~> ";
-     cout << sqrt(site_two * intial_range + 910 - 83);  
-}
-
-if (cin >> site_three) {
-    cout << "Radar impulsive to double range ~~> ";
-      cout << sqrt(site_three * radar_range + intial_range / 910 + 83);
-}
-
-
-if (cin >> site_four) {
-    cout << "Radar impulsive to double range ~~> ";
-      cout << sqrt(site_four * radar_range + intial_range / 910 + 83);
-}
-
+    return 0;
 }
